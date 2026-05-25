9657237a567b0d09f60784b28770745e37c3d8d9
YIMANIS-FULL <yimocuba@gmail.com>
2026-05-25 03:57:16 -0400

    update webmanifest

--- app/layout.js
+++ app/layout.js
@@ -106,9 +106,14 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        {/* PUESTO DIRECTAMENTE AQUÍ COMO HTML TRADICIONAL */}
-        <meta name="impact-site-verification" content="eb4a42e1-fd1c-4174-ac97-610a0115fd4b" />
-        
+        {/* SCRIPT DE IMPACT FORMATEADO PARA NEXT.JS */}
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `(function(i,m,p,a,c,t){c.ire_o=p;c[p]=c[p]||function(){(c[p].a=c[p].a||[]).push(arguments)};t=a.createElement(m);var z=a.getElementsByTagName(m)[0];t.async=1;t.src=i;z.parentNode.insertBefore(t,z)})('https://utt.impactcdn.com/P-A7315492-7421-4ec9-b58b-e926641898581.js','script','impactStat',document,window);impactStat('transformLinks');impactStat('trackImpression');`
+          }}
+        />
+
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLdSchema) }}
